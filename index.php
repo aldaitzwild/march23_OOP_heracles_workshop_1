21de7720fc887ae877379d6a8eb76c10a626f2fa
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-03-28 13:47:29 +0200

    Add the correction for the workshop

--- index.php
+++ index.php
@@ -1,4 +1,32 @@
 <?php
 
-// First Labour : Heracles vs Nemean Lion
-// use your Figher class here
+require 'src/Fighter.php';
+
+$heracles = new Fighter('Héraclès', 20, 6);
+$lion = new Fighter('Lion de Némée', 11, 13);
+$round = 1;
+
+while(true) {
+    echo "#round $round\n";
+    echo $heracles->getName() . " - PV : " . $heracles->getLife() . "\n";
+    echo $lion->getName() . " - PV : " . $lion->getLife() . "\n";
+
+    $heracles->fight($lion);
+
+    if(!$lion->isAlive()) {
+        echo $lion->getName() . " est mort !\n";
+        echo $heracles->getName() . " a gagné !\n";
+        break;
+    }
+
+    $lion->fight($heracles);
+
+    if(!$heracles->isAlive()) {
+        echo $heracles->getName() . " est mort !\n";
+        echo $lion->getName() . " a gagné !\n";
+        break;
+    }
+
+    $round++;
+}
+
--- src/Fighter.php
+++ src/Fighter.php
@@ -1,3 +1,46 @@
 <?php
 
-/* Fighter class definition */
+class Fighter 
+{
+    private string $name;
+    private int $strength;
+    private int $dexterity;
+    private int $life;
+
+    public const MAX_LIFE = 100;
+
+    public function __construct(string $name, int $strength, int $dexterity)
+    {
+        $this->name = $name;
+        $this->strength = $strength;
+        $this->dexterity = $dexterity;
+        $this->life = self::MAX_LIFE;
+    }
+
+    public function fight(Fighter $opponent): void
+    {
+        $attack = rand(1, $this->strength);
+        $damage = $attack - $opponent->dexterity;
+
+        if($damage < 0) $damage = 0;
+
+        $opponent->life -= $damage;
+
+        if($opponent->life < 0) $opponent->life = 0;
+    }
+
+    public function getName() : string
+    {
+        return $this->name;
+    }
+
+    public function getLife() : int
+    {
+        return $this->life;
+    }
+
+    public function isAlive() : bool
+    {
+        return $this->getLife() > 0;
+    }
+}
